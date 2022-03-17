d14401e803c0a2b50ecac9789adc1d1bbb2ec6c0
gz <gas@cock.li>
2022-03-17 17:08:45 +0200

    Test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM node:14.17.0-alpine3.13 AS build
+FROM node:13.7.0-alpine3.10 AS build
 WORKDIR /build
 COPY . .
-RUN npm install -g typescript; \ 
+RUN npm install -g typescript@3.8; \ 
         yarn; yarn build
 
-FROM node:14.17.0-alpine3.13
+FROM node:13.7.0-alpine3.10
 WORKDIR /app
-COPY --from=build /build/dist/js/ /app
-CMD [ "node", "/app/app.js" ]
+COPY --from=build /build/node_modules /app/node_modules
+COPY --from=build /build/dist /app/dist
+CMD [ "node", "dist/js/app.js" ]
--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
 
